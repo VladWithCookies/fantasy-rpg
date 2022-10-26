bcfc491bb92c9809d381ff99cb4b27ad9106ff35
Vlad Viderko <summervibeswith9pm@gmail.com>
2022-10-26 15:49:48 +0200

    Add background

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const SCREEN_HEIGHT = window.innerHeight;
 
 export const DEFAULT_X_VELOCITY = 350;
 export const DEFAULT_Y_VELOCITY = 800;
+
+export const TILE_SIZE = 128;
--- src/scenes/Dungeon.js
+++ src/scenes/Dungeon.js
@@ -1,7 +1,8 @@
 import { Scene } from 'phaser';
 
+import { SCREEN_HEIGHT, DEFAULT_X_VELOCITY, DEFAULT_Y_VELOCITY, TILE_SIZE } from 'constants';
 import background from 'assets/dungeon/background.png';
-import { SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_X_VELOCITY, DEFAULT_Y_VELOCITY } from 'constants';
+import ground from 'assets/dungeon/ground.png';
 
 export default class Dungeon extends Scene {
   constructor() {
@@ -11,24 +12,44 @@ export default class Dungeon extends Scene {
   preload() {
     this.load.image('player', 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Pan_Blue_Circle.png');
     this.load.image('background', background);
+    this.load.image('ground', ground);
   }
 
   create() {
-    console.log(background);
-    this.container = this.add.container(SCREEN_WIDTH, SCREEN_HEIGHT);
-    this.background = this.add.tileSprite(0, 0, 32, 32, 'background');
+    const width = 2000;
+    const height = SCREEN_HEIGHT;
+    const centerX = width / 2;
+    const centerY = height / 2;
 
-    this.container.add(this.background);
+    const groundY = height - TILE_SIZE / 2;
+    const groundWidth = width + TILE_SIZE / 4;
 
-    this.player = this.physics.add
-      .sprite(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 'player')
-      .setScale(0.1);
+    const backgroundX = - centerX;
+    const backgroundY = - (centerY + 8);
 
+    const container = this.add.container(width, height);
+    const background = this.add.tileSprite(backgroundX, backgroundY, width, height, 'background');
+
+    container.add(background);
+
+    this.ground = this.add.tileSprite(centerX, groundY, groundWidth, TILE_SIZE, 'ground');
+    this.physics.add.existing(this.ground);
+    this.ground.body.immovable = true;
+    this.ground.body.allowGravity = false;
+
+    this.player = this.physics.add.sprite(200, 200, 'player').setScale(0.1);
     this.player.setCollideWorldBounds(true);
+
+    this.physics.world.setBounds(0, 0, width, height);
+    this.cameras.main.setBounds(0, 0, width, height);
+    this.cameras.main.startFollow(this.player, true, 0.5, 0.5);
+
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+    this.physics.collide(this.player, this.ground);
+
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-DEFAULT_X_VELOCITY);
     } else if (this.cursors.right.isDown) {
